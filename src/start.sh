3e5b1f5bbd841f19b579779b434b9892fe9d4e0e
Marc Gueury <marc.gueury@oracle.com>
2026-01-30 20:17:25 +0100

    v3.0

--- src/start.sh
+++ src/start.sh
@@ -3,4 +3,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 . ./env.sh
 
-npx -y @maximhq/bifrost app-dir ./bifrost-data > app.log 2>&1 
+npx -y @maximhq/bifrost -host 0.0.0.0 -app-dir ./bifrost-data > app.log 2>&1 
